afb2725f29911fc80a713694aab93310f19f59b2
Keurig <64445183+Keurig-Creator@users.noreply.github.com>
2022-12-09 23:04:44 -0600

    added input

--- src/main/java/me/keurig/repositorytest/Main.java
+++ src/main/java/me/keurig/repositorytest/Main.java
@@ -4,6 +4,8 @@ import me.keurig.repositorytest.version.Version;
 import me.keurig.repositorytest.version.Version_1;
 import me.keurig.repositorytest.version.Version_2;
 
+import java.util.Scanner;
+
 public class Main {
     
     // Version
@@ -16,7 +18,25 @@ public class Main {
     }
 
     private void init() {
-        version = new Version_2();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a version, 1 or 2.");
+
+        while (true) {
+            String input = scanner.next();
+            input = input.toLowerCase();
+
+            if (input.equals("1")) {
+                version = new Version_1();
+                break;
+            } else if (input.equals("2")) {
+                version = new Version_2();
+                break;
+            } else {
+                System.out.println("Invalid input! 1 or 2.");
+            }
+        }
+
         version.print();
     }
 }
